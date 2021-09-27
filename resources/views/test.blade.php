bab3468ed2a37f5cea05316da7dac95312314680
kurakawaryunosuke <ryu.baseball6@icloud.com>
2021-09-27 15:31:39 +0900

    laravel

---
+++ resources/views/test.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  @if($index%2==0)
+  2の倍数です。
+  @elseif($index%3==0)
+  ３の倍数です。
+  @else
+  ２の倍数でも３の倍数でもありません。
+  @endif
+</body>
+</html>
